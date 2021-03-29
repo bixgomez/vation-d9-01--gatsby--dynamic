9ba57edda40acdeda4a7eb1d3ffbd3e2d6892e4c
Richard Gilbert <gilbert@Melies.local>
2021-03-29 15:02:19 -0700

    updating random person page.

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,12 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 import { RandomPerson } from "../modules/RandomPerson"
 import { Router as MyRouter } from "@reach/router"
 
 const Router = () => {
   return (
-    <MyRouter>
-      <RandomPerson path="/app/random-person/" />
-    </MyRouter>
+    <Layout>
+      <MyRouter>
+        <RandomPerson path="/app/random-person/" />
+      </MyRouter>
+    </Layout>
   )
 }
 
